b49c15e8927289af14355a531ddcb40a0942ba36
OmarZayed77 <zayedomarr@gmail.com>
2019-03-28 20:22:03 +0200

    Require Cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const productssRouter = require('./routes/products');
@@ -10,7 +11,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
