700af59327adf298ca02e47b7e46831070843b27
OmarZayed77 <zayedomarr@gmail.com>
2019-03-29 14:11:21 +0200

    Modify picture prop to image

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('../DBL/mongoose');
 
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    picture: String,
+    image: String,
     categoryId: String,
     addedBy: {type: String, required: true},
     isOnSale: {type: Boolean, required: true},
