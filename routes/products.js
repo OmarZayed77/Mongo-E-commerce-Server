93dd91fab9cba0f413c80de058976038f8ff1bf0
OmarZayed77 <zayedomarr@gmail.com>
2019-04-04 16:41:38 +0200

    Check the product is deleted

--- routes/products.js
+++ routes/products.js
@@ -47,8 +47,9 @@ router.delete('/:productId', async (req, res, next)=> {
     const product = await productsDB.getById(req.params.productId).catch(console.error);
     if(!product) return next(createError(404));
     if(req.user._id.toHexString() !== product.addedBy) return next(createError(401));
-    else await productsDB.delete(req.params.id).catch(console.error);
-    res.send("product was deleted successfully");
+    else product =  await productsDB.delete(req.params.id).catch(console.error);
+    if(!product) return next(createError(404));
+    else res.send("product was deleted successfully");
 });
 
 router.patch('/:id', async (req, res, next)=> {
